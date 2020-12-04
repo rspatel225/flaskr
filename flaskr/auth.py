0e21a8669477f2ab6eb918978bf57e2d761bb5db
Ravi Patel <ravipatel225@gmail.com>
2020-12-04 15:24:48 -0600

    login and register templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -18,7 +18,7 @@ def register():
             error = 'Username is required'
         elif not password:
             error = 'Password is required'
-        elif db.execute('SELECT id FROM user WHERE username = ?', (username)).fetchone() is not None:
+        elif db.execute('SELECT id FROM user WHERE username = ?', (username,)).fetchone() is not None:
             error = f'User {username} is already registered'
 
         if error == None:
@@ -42,7 +42,7 @@ def login():
         error = None
         user = db.execute(
             'SELECT * FROM user WHERE username == ?', 
-            (username) 
+            (username,) 
         ).fetchone()
 
         if user is None:
@@ -68,7 +68,7 @@ def load_logged_in_user():
     else:
         g.user = get_db().execute(
             'SELECT * FROM user WHERE id = ?', 
-            (user_id)
+            (user_id,)
         ).fetchone()
 
 @bp.route('/logout')
